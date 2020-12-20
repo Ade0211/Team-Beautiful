903c668ec5db01f189d0c365e4ae3dca9105031f
Ade <62720742+Ade0211@users.noreply.github.com>
2020-12-20 17:48:16 +0000

    sorted conflicts in home.js and GraduateInformationDiplay.js

--- client/src/components/graduates/GraduateInformationDisplay.js
+++ client/src/components/graduates/GraduateInformationDisplay.js
@@ -5,7 +5,23 @@ import { LinkContainer } from "react-router-bootstrap";
 import ShowMoreText from "react-show-more-text";
 
 
+// const GraduateInformationDisplay = ({ filteredData }) => {
+//   const [show, setShow] = useState(false);
 
+
+//   //extracting the root domain here
+//   //if there is a subdomain
+//   if (arrLen > 2) {
+//       domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+//       //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
+//       if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+//           //this is using a ccTLD
+//           domain = splitArr[arrLen - 3] + '.' + domain;
+//       }
+//   }
+//   return domain;
+
+// }
 
 const GraduateInformationDisplay = () => {
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 import "../../bootstrap.min.css";
 import "../../index.css";
 import {
-  Container,
-  Row,
-  Col,
-  InputGroup,
-  FormControl,
+  // Container,
+  // Row,
+  // Col,
+  // InputGroup,
+  // FormControl,
   Button,
 } from "react-bootstrap";
-
+// import logo from "../utility/logo.png"
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import ShowMoreText from "react-show-more-text";
@@ -31,8 +31,7 @@ const Home = () => {
 
   const [location, setLocation] = useState("");
   const [language, setLanguage] = useState("");
-  const [currentQuote, setCurrentQuote] = useState();
-  const getRandIndex = arr => Math.floor(Math.random() * arr.length);
+  // const getRandIndex = arr => Math.floor(Math.random() * arr.length);
 
   /**
    * Select a quote at random and remove from the current list of
@@ -124,36 +123,15 @@ const Home = () => {
     );
 
   console.log(filteredData);
-  const randomShuffle = shuffleArray(filteredData);
-
-//see if it works
-// useEffect(() => {
-//   const i = setInterval(() => {
-//      setData(([first, ...rest]) => [...rest, first]);
-//   }, 1000);
-
-//   return () => clearInterval(i);
-// }, []);
-// console.log(data);
-// console.log(currentQuote);
 
 
   return (
     <div className="container-fluid navbar-expand-lg">
-      {/* <div className="card-header container-fluid">
-        <img
-          className="logo bg-light rounded"
-          src={logo}
-          alt="header-logo"
-          style={{ width: "5rem" }}
-        />
-        <div className="d-flex justify-content-end">
-          <h6 className="mr-4">CYF Login</h6>
-          <h6>Student Login</h6>
-        </div>
-      </div> */}
+       <div className="card-header container-fluid">
 
-      <div className="container">
+        <div className="d-flex justify-content-end">
+
+           <div className="container">
         <h4 className="display-3">Graduates Directory</h4>
         <p>
           Our amazing and talented graduates are looking for new opportunities.
@@ -185,39 +163,6 @@ const Home = () => {
                 />
               </div>
             </div>
-            <div className="col-8 ml-4">
-              <div className="form-col">
-                <label class="checkbox-inline mr-3">
-                  <input
-                    type="checkbox"
-                    name="full_time"
-                    value={full_time}
-                    onChange={handleCheckBox}
-                  />
-                  Full-Time
-                </label>
-                <label class="checkbox-inline mr-3">
-                  <input
-                    class="form-control mr-sm-2"
-                    type="text"
-                    name="Location"
-                    value={location}
-                    onChange={handleLocation}
-                    placeholder="Location"
-                    aria-label="Search"
-                  />
-                </div>
-                <div class="form-group">
-                  <input
-                    class="form-control mr-sm-2"
-                    type="text"
-                    placeholder="Language"
-                    aria-label="Search"
-                    value={language}
-                    onChange={handleLanguage}
-                  />
-                </div>
-              </div>
               <div className="col-8 ml-4">
                 <div className="form-col">
                   <label class="checkbox-inline mr-3">
@@ -241,8 +186,9 @@ const Home = () => {
                   <label class="checkbox-inline mr-3">
                     <input
                       type="checkbox"
-                      // name="internship"
-                      // value={internship}
+                      name="internship"
+                      value={internship}
+                      onChange={handleCheckBox}
                     />
                     Internship
                   </label>
@@ -273,142 +219,21 @@ const Home = () => {
                       type="checkbox"
                       onChange={handleCheckBox}
                     />
-                    Temp/Contract
+                    Contract
                   </label>
                 </div>
               </div>
             </div>
-          </div> */}
+            </div>
+            </div>
         </div>
       </div>
 
-      <div className="container d-flex justify-content-space-evenly flex-wrap">
-        {randomShuffle.length > 0 &&
-          randomShuffle.map((obj, _id) => {
-            return (
-              <div
-                className="col  d-flex justify-content-space-evenly mb-5 "
-                key={obj._id}
-              >
-                <div
-                  class="row border border-dark mx -3 "
-                  style={{ width: "30rem" }}
-                >
-                  <div className="card-body ">
-                    <h4 className="float-right">
-                      {
-                        <Link to={`/graduates/${obj._id}/profile`}>
-                          <i class="fas fa-share-square"></i>
-                        </Link>
-                      }
-                    </h4>
-                    <h5 class="card-title text-center text-info">{obj.fullname}</h5>
-                    <p>Languages:{obj.languages}</p>
-                    <p> HeadLine: {obj.headline}</p>
-                    <hr />
-                    <p class="card-text">Location:{obj.current_location}</p>
 
-                    <div class="row justify-content-around">
-                      <div class="col-6">
-                        <button
-                          type="button"
-                          class="btn btn-warning btn-sm mr-1"
-                        >
-                          {" "}
-                          Open to relocation
-                        </button>
-                      </div>
-                      <div class="col-5">
-                        <button
-                          type="button"
-                          class="btn btn-success btn-sm mr-1"
-                        >
-                          {" "}
-                          Open to remote
-                        </button>
-                      </div>
-                    </div>
 
-                    <p class="card-text">{obj.full_time ? "Full Time" : ""}</p>
-                    <p class="card-text">{obj.part_time ? "Part Time" : ""}</p>
-                    <p class="card-text">
-                      {obj.willing_relocate ? "Open to Relocate" : ""}
-                    </p>
-                    <p class="card-text">
-                      {obj.willing_remote ? "Open to Remote" : ""}
-                    </p>
 
-                    <hr />
 
-                    <div className="container">
-                      <div class="row justify-content-around ">
-                    <div class="col-4">
-                          <i class="fab fa-linkedin fa-2x"></i>
 
-                          <a
-                            className="ml-2"
-                            style={{ fontSize: "65%" }}
-                            target="_blank"
-                            rel="noreferrer"
-                            href={"http://"+ obj.linkedin}
-                          >LinkedIn</a>
-                        </div>
-                     <div class="col-4 mt-1">
-                          <i class="fas fa-globe fa-2x"> </i>
-
-                          <a
-                            className="ml-2"
-                            style={{ fontSize: "70%" }}
-                            href={"http://"+ obj.website}
-                          >Website</a>
-                        </div>
-                      </div>
-
-                      <div class="row justify-content-around  ">
-                     <div class="col-4 ">
-                          <i class="fas fa-file-csv fa-2x"></i>
-                          <a className="ml-2" style={{ fontSize: "80%" }} href={"http://"+ obj.upload_cv} >cv</a>
-
-                        </div>
-                       <div class="col-4 mt-1">
-                          <i class="fab fa-github fa-2x"></i>
-                          <a className="ml-2" style={{ fontSize: "70%" }} href={"http://"+ obj.github}> GitHub</a>
-
-                        </div>
-                      </div>
-                    </div>
-
-                    <hr />
-                    <ShowMoreText
-                      lines={3}
-                      more={
-                        <button type="button" class="btn btn-primary btn-sm">
-                          Show more
-                        </button>
-                      }
-                      less={
-                        <button type="button" class="btn btn-primary btn-sm">
-                          Show less
-                        </button>
-                      }
-                      className="content-css"
-                      anchorClass="my-anchor-css-class"
-                      onClick={executeOnClick}
-                      expanded={false}
-                      width={350}
-                    >
-
-                      <p class="card-subtitle mb-2 text-dark">
-                        {obj.resume_textarea}
-                      </p>
-                    </ShowMoreText>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </div> */}
-      {/* <Footer /> */}
 
       <div className="container">
         <div className="row mb-2">
@@ -557,22 +382,11 @@ const Home = () => {
             })}
         </div>
       </div>
-    </>
+      </div>
+
   );
 };
 
-// const Footer = () => {
-//   return (
-//     <footer>
-//       <Container>
-//         <Row>
-//           <Col className="text-center py-3">
-//             Copyright &copy; 2020 Code Your Future
-//           </Col>
-//         </Row>
-//       </Container>
-//     </footer>
-//   );
-// };
+
 
 export default Home;
